c87e6ed5319317af2c34ab8a21333c598d71d56a
luftboud <ijamaharyta@gmail.com>
2023-11-13 21:57:05 +0200

    fixed funcs
    have to do loader and modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,40 +7,43 @@ const API_KEY = '36975406-14cef0b651718033f414d4154';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 class App extends Component {
   state = {
-    q: '',
+    q: 'random',
     page: 1,
+    hits: []
   };
-  hitsArr = [];
   handleSearch = evt => {
     evt.preventDefault();
     const input = evt.target.querySelector('input');
     const request = input.value;
-    this.setState({ q: request });
-    // evt.target.reset();
+    this.setState({ q: request, hits: [], page: 1 });
   };
   handleLoad = () => {
     this.setState({ page: this.state.page + 1 });
   };
-  // async componentDidMount() {
-  //   const response = await axios.get(
-  //     `/?key=${API_KEY}&q=${this.state.q}&page=1&image_type=photo&orientation=horizontal&per_page=12`
-  //   );
-  //   this.setState({ articles: response.data.hits });
-  //   this.hitsArr = this.state.articles;
-  // }
-  async componentDidUpdate() {
-    console.log(this.state.page);
+  async componentDidMount() {
     const response = await axios.get(
-      `/?key=${API_KEY}&q=${this.state.q}&page=${this.state.page}&image_type=photo&orientation=horizontal&per_page=12`
+      `/?key=${API_KEY}&q=${this.state.q}&page=1&image_type=photo&orientation=horizontal&per_page=12`
     );
-    response.data.hits.map(el => this.hitsArr.push(el));
-    console.log(this.hitsArr);
+    const hits = response.data.hits;
+      this.setState({hits: [...hits]})
+  }
+  async componentDidUpdate(prevProps , prevState) {
+    if (this.state.page !== prevState.page || this.state.q !== prevState.q) {
+          console.log(this.state.page);
+      const response = await axios.get(
+        `/?key=${API_KEY}&q=${this.state.q}&page=${this.state.page}&image_type=photo&orientation=horizontal&per_page=12`
+      );
+      const hits = response.data.hits;
+      this.setState(prevState => ({
+        hits: [...prevState.hits, ...hits],
+      }));
+    }
   }
   render() {
     return (
       <div className="app">
         <Searchbar onSubmit={this.handleSearch}></Searchbar>
-        <ImageGallery hits={this.hitsArr}></ImageGallery>
+        <ImageGallery hits={this.state.hits}></ImageGallery>
         <Button onClick={this.handleLoad}></Button>
       </div>
     );
