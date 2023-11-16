0e5e83ee99a70057c76c3cb55944e152a3f2b9aa
luftboud <ijamaharyta@gmail.com>
2023-11-16 19:45:06 +0200

    loader done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import { Button } from './Button';
+import { Rings } from  'react-loader-spinner'
 const API_KEY = '36975406-14cef0b651718033f414d4154';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 class App extends Component {
@@ -11,11 +12,14 @@ class App extends Component {
     page: 1,
     hits: []
   };
+  loader_boolean = true
+  button_display = "unvisible";
   handleSearch = evt => {
     evt.preventDefault();
     const input = evt.target.querySelector('input');
     const request = input.value;
     this.setState({ q: request, hits: [], page: 1 });
+    evt.target.reset();
   };
   handleLoad = () => {
     this.setState({ page: this.state.page + 1 });
@@ -24,15 +28,22 @@ class App extends Component {
     const response = await axios.get(
       `/?key=${API_KEY}&q=${this.state.q}&page=1&image_type=photo&orientation=horizontal&per_page=12`
     );
+    this.loader_boolean = false;
+    this.button_display = "visible";
     const hits = response.data.hits;
-      this.setState({hits: [...hits]})
+    this.setState({ hits: [...hits] })
+    
   }
-  async componentDidUpdate(prevProps , prevState) {
+  async componentDidUpdate(prevProps, prevState) {
+    this.loader_boolean = true;
+    this.button_display = "unvisible";
     if (this.state.page !== prevState.page || this.state.q !== prevState.q) {
           console.log(this.state.page);
       const response = await axios.get(
         `/?key=${API_KEY}&q=${this.state.q}&page=${this.state.page}&image_type=photo&orientation=horizontal&per_page=12`
       );
+      this.loader_boolean = false;
+      this.button_display = "visible";
       const hits = response.data.hits;
       this.setState(prevState => ({
         hits: [...prevState.hits, ...hits],
@@ -43,8 +54,18 @@ class App extends Component {
     return (
       <div className="app">
         <Searchbar onSubmit={this.handleSearch}></Searchbar>
+        <Rings
+          height="160"
+          width="160"
+          color="#3f51b5"
+          radius="6"
+          wrapperStyle={{ margin: "auto", paddingTop: 310}}
+          wrapperClass=""
+          visible={this.loader_boolean}
+          ariaLabel="rings-loading"
+        />
         <ImageGallery hits={this.state.hits}></ImageGallery>
-        <Button onClick={this.handleLoad}></Button>
+        <Button onClick={this.handleLoad} display={this.button_display}></Button>
       </div>
     );
   }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
-const Button = ({ onClick }) => {
+const Button = ({ onClick, display }) => {
   return (
-    <button type="button" onClick={onClick}>
+    <button type="button" onClick={onClick} className={display}>
       Load more
     </button>
   );
 };
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
 export { Button };
