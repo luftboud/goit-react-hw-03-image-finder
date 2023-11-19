59a5e56a508eed14a7c502d90ae03a6d5b05bbf2
luftboud <ijamaharyta@gmail.com>
2023-11-19 23:21:45 +0200

    ..

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,4 +1,4 @@
-import PropTypes, { func } from 'prop-types';
+import PropTypes  from 'prop-types';
 import css from './styles/Searchbar.module.css'
 const Searchbar = ({ onSubmit }) => {
  function handleSearch (evt) {
